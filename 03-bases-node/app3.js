39575b2c995f4b5fdd33c2ad8f8460922ad225cf
Alejandro Agudelo Mejia <alejandro.agudelo@gmail.com>
2020-02-24 22:58:04 +0100

    Yargs

--- 03-bases-node/app3.js
+++ 03-bases-node/app3.js
@@ -6,3 +6,10 @@ let parametro = argv[2];
 let base = parametro.split('=')[1];
 crearArchivo(base).then(archivo => console.log(`Archivo creado: ${archivo}`))
     .catch(e => console.log(e));
+
+//npm init
+//npm i yargs --save
+//ignore node_modules
+//npm install --save-dev nodemon 
+//--save-dev --> utilizado solo para la aplicación local y no requiere permisos del sistema porque no esta de forma global
+//npm install
