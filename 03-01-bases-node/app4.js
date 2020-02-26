6e121c6b012f8126422f77982e823f8705e1c5e7
Alejandro Agudelo Mejia <alejandro.agudelo@gmail.com>
2020-02-26 06:56:02 +0100

    yargs comando crear

--- 03-01-bases-node/app4.js
+++ 03-01-bases-node/app4.js
@@ -1,5 +1,16 @@
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar')
 const argv = require('yargs')
-    .command('listar', 'Imprime en consla la tabla de multiplicar', {
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .command('crear', 'Crea el fichero de la tabla de multiplicar', {
         base: {
             demand: true,
             alias: 'b'
@@ -15,6 +26,26 @@ console.log(argv);
 console.log(argv.base);
 console.log(argv.limite);
 
+
+
+let comando = argv._[0];
+switch (comando) {
+    case 'listar':
+        //console.log('listar');
+        let baseListar = argv.base;
+        let limiteListar = argv.limite;
+        listarTabla(baseListar, limiteListar).then(msg => console.log(msg)).catch(e => console.log(e));
+        break;
+    case 'crear':
+        //console.log('crear');
+        let base = argv.base;
+        crearArchivo(base).then(archivo => console.log(`Archivo creado: ${archivo}`))
+            .catch(e => console.log(e));
+        break;
+    default:
+        console.log('Comando no reconocido');
+}
+
 //node app4 listar
 //node app4 listar --base 5
 
@@ -24,3 +55,8 @@ console.log(argv.limite);
 //npm install --save-dev nodemon 
 //--save-dev --> utilizado solo para la aplicación local y no requiere permisos del sistema porque no esta de forma global
 //npm install
+/*
+Yargs guarda objetos sin pares de valores de tal forma que podrian
+ser usados internamente, ejemplo: node app crear --base 20, donde crear 
+sería el objeto que yargs guardaría
+*/
--- 03-01-bases-node/multiplicar/multiplicar.js
+++ 03-01-bases-node/multiplicar/multiplicar.js
@@ -1,26 +1,46 @@
 const fs = require('fs');
 let crearArchivo = (base) => {
-        return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
 
-            if (!Number(base)) {
-                reject(`${base} no es un numero`);
-                return;
-            }
+        if (!Number(base)) {
+            reject(`${base} no es un numero`);
+            return;
+        }
 
-            let data = '';
-            for (let i = 1; i <= 10; i++) {
-                data += `${base} * ${i} = ${base*i}\n`;
-            }
+        let data = '';
+        for (let i = 1; i <= 10; i++) {
+            data += `${base} * ${i} = ${base*i}\n`;
+        }
 
-            fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
-                if (err) reject(err);
-                else
-                    resolve(`tabla-${base}.txt`);
-            })
-        });
-    }
-    //process --> listado de comandos que tienen el node
-    //process.argv --> utilizado para recibir y argumentos de la consola
+        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+            if (err) reject(err);
+            else
+                resolve(`tabla-${base}.txt`);
+        })
+    });
+}
+
+let listarTabla = (base, limite = 10) => {
+    return new Promise((resolve, reject) => {
+        if (!Number(base)) {
+            reject(`${base} no es número`);
+            return;
+        }
+        if (!Number(limite)) {
+            reject(`${limite} no es número`);
+            return;
+        }
+        let data = '';
+        for (let i = 1; i <= limite; i++) {
+            data += `${base} * ${i} = ${base*i}\n`;
+        }
+        resolve(data);
+    })
+}
+
+//process --> listado de comandos que tienen el node
+//process.argv --> utilizado para recibir y argumentos de la consola
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listarTabla
 }
