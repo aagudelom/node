dfa9ffa87bc13edbf6e2906dd7a80a598073e6f3
aagudelom <alejandro.agudelo@gmail.com>
2020-11-29 18:45:41 +0100

    hbs parciales

---
+++ 06-webserver-2/server4.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+app.use(express.static(__dirname + '/public'));
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine','hbs');
+
+app.get('/',(req,res) => {
+    res.render('home',{
+        nombre:'Alejandro',
+        anyo:new Date().getFullYear()
+    });
+
+});
+app.get('/about',(req,res) => {
+    res.render('about',{        
+        anyo:new Date().getFullYear()
+    });
+
+});
+
+//el app listen permite utilizar una función
+app.listen(3000, () => {
+    console.log('Escuchando peticiones en el puerto 3000');
+});
