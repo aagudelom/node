126b5c36bb6ae0a38a1c425dfe351aeadc6fb02d
aagudelom <alejandro.agudelo@gmail.com>
2020-12-06 17:28:17 +0100

    heroku

--- 06-webserver-3/server.js
+++ 06-webserver-3/server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/public'));
 hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine','hbs');
 
-
+const port = process.env.PORT || 3000;
 
 app.get('/',(req,res) => {
     res.render('home',{
@@ -21,6 +21,6 @@ app.get('/about',(req,res) => {
 });
 
 //el app listen permite utilizar una función
-app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puerto 3000');
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
 });
