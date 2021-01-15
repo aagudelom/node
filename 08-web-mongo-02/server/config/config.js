ef62bbb4b64fa8b4fdbe2ab7130908200bf9eabc
aagudelom <alejandro.agudelo@gmail.com>
2021-01-15 18:46:22 +0100

    conectando a mongo

---
+++ 08-web-mongo-02/server/config/config.js
@@ -0,0 +1,3 @@
+// PUERTO
+process.env.PORT = process.env.PORT || 3000;
+process.env.URLDB = process.env.URLDB || 'mongodb://localhost:27017/test';
---
+++ 08-web-mongo-02/server/server.js
@@ -0,0 +1,57 @@
+require('./config/config')
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.get('/usuario', function (req, res) {
+  res.json('get World')
+});
+
+app.post('/usuario', function (req, res) {
+    let body = req.body;
+    if(body.nombre === undefined) {
+        res.status(400).json({
+            ok:false,
+            mensaje: 'El nombre es obligatorio'
+        })
+    } else {
+        res.json({
+            body
+        });
+    }
+});
+
+app.put('/usuario/:id', function (req, res) {
+    let codigo = req.params.id;
+    res.json({codigo})
+});
+
+app.delete('/usuario', function (req, res) {
+    res.json('delete World')
+});
+ 
+app.listen(process.env.PORT, () => {
+    console.log(`Escuando puerto ${process.env.PORT}`);
+});
+
+mongoose.connect(process.env.URLDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}, (err, resp) => {
+    if(err) {
+        throw err;
+    }
+    console.log("Conectado a la base de datos");
+});
